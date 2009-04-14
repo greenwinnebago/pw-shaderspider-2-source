d0be4f04d64eac43b0d387e173898918799c5a0c
dan.nicho <dan.nicho@9f0050e8-28a6-11de-83d2-3711199b9f83>
2009-04-14 04:12:05 +0000

    Removed bad paths

--- trunk/Runtime/Python/Poseworks/ShaderSpider/wacros/Copy_To_Matching.py
+++ trunk/Runtime/Python/Poseworks/ShaderSpider/wacros/Copy_To_Matching.py
@@ -1,10 +1,8 @@
 import os, sys, poser, string
 
 os.chdir( os.path.dirname(poser.AppLocation()) )
-if float(poser.Version()) >= 7:
-    sys.path.append(os.path.join("Runtime","Python","PoseWorks","ShaderSpider","Data","24"))
-else:
-    sys.path.append(os.path.join("Runtime","Python","PoseWorks","ShaderSpider","Data","22"))
+
+sys.path.append(os.path.join("Runtime","Python","PoseWorks","ShaderSpider","Data"))
 
 import ss6Node
 
